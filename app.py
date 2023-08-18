4ac358c63a801a5705c0d229237aeb1a33b15549
Sk4467 <skshahid4467@gmail.com>
2023-08-19 02:53:09 +0530

    Added streamlit app for HF translator

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+# Load model and tokenizer from Hugging Face Model Hub
+MODEL_NAME = "Sk4467/Bengali_translator"  # Replace with your model's path on Hugging Face
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
+
+st.title("English to Bengali Translation")
+
+# User input
+english_text = st.text_area("Enter English text:", "")
+
+if english_text:
+    # Encode the text and generate translation
+    encoded = tokenizer.encode(english_text, return_tensors="pt")
+    translation_ids = model.generate(encoded)
+    bengali_translation = tokenizer.decode(translation_ids[0], skip_special_tokens=True)
+    
+    # Display the translation
+    st.write("Bengali Translation:")
+    st.write(bengali_translation)
+
